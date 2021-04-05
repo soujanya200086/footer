878319dc61b5c1d074477956d31ac820015aba96
soujanya200086 <78073602+soujanya200086@users.noreply.github.com>
2021-04-05 14:01:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,132 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<body>
+    <div class="footer_position">
+     
+    <div class="main_footer">
+   
+
+    
+      <div class="footer_partone">
+        <div class="container">
+          <div class="row">
+            
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+              <h6><a href="tel:(+91) 9503765844"><i class="fa fa-mobile"></i> (+91) 9503765844</a></h6>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+              <h6><a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRwRCQzwmTQCKFKgfNJdcxCRlHflTghLrTGKxFQwSQCWxskzMWStlfhDHFBkMcnbGDLNWzdC"><i class="fa fa-envelope"></i> support@sillicon14.com</a></h6>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+              <h6><a href="https://www.google.com/maps/place/15%C2%B022'14.9%22N+75%C2%B007'20.7%22E/@15.370791,75.122424,13z/data=!4m5!3m4!1s0x0:0x0!8m2!3d15.3707907!4d75.1224238?hl=en-GB"><i class="fa fa-map-marker"></i> First Floor, Startup Street, R.H.Kulkarni Building,
+K.L.E Technological University, Vidyanagar,
+Hubli.</a></h6>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+              <div class="footer_newsletterbox">
+               
+                <div class="clr"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer_parttwo">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+              <div class="footerbox_menulist">
+                <h4>Main Menu</h4>
+                <ul>
+                  <li><a href="https://silicon14.ml/"><i class="fa fa-angle-double-right"></i>   Home</a></li>
+                  <li><a href="https://silicon14.ml/about/"><i class="fa fa-angle-double-right"></i>   About Us</a></li>
+                  <li><a href="#"><i class="fa fa-angle-double-right"></i>Portfolio</a></li>
+                  <li><a href="https://silicon14.ml/"><i class="fa fa-angle-double-right"></i>Blog</a></li>
+                  <li><a href="https://silicon14.ml/contact"><i class="fa fa-angle-double-right"></i>Contact Us</a></li>
+                </ul>
+              </div>
+            </div>
+         
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+              <div class="footerbox_menulist">
+                <h4>Digital Marketing</h4>
+                <ul>
+                  <li><a href="#"><i class="fa fa-angle-double-right"></i>SEO Services</a></li>
+                  <li><a href="#"><i class="fa fa-angle-double-right"></i>SMO Services</a></li>
+                  <li><a href="#"><i class="fa fa-angle-double-right"></i>Pay Per Click</a></li>
+                  <li><a href="#"><i class="fa fa-angle-double-right"></i>Email Marketing</a></li>
+                  <li><a href="#"><i class="fa fa-angle-double-right"></i>Content Writing</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+              <div class="footerbox_menulist">
+              <h4>About the company</h4>
+					<h2>Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.</h2>
+			
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer_partthree">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+              <div class="socialicons">
+                <ul>
+                  <li><span>Follow Us :</span></li>
+                  <li><a href="https://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+                  <li><a href="https://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+                  <li><a href="https://www.linkedin.com/"><i class="fa fa-linkedin"></i></a></li>
+
+                  <li><a href="https://www.instagram.com/"><i class="fa fa-instagram"></i></a></li>
+                 
+                  <li><a href="https://youtube.com/"><i class="fa fa-youtube"></i></a></li>
+                  <div class="clr"></div>
+                </ul>
+              </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+              <div class="payment_card">
+                <ul>
+                  <li><span>Payment Accept :</span></li>
+                  <li><a href="https://www.visa.co.in"><i class="fa fa-cc-visa">&nbsp;</i></a></li>
+                  <li><a href="https://www.mastercard.co.in"><i class="fa fa-cc-mastercard">&nbsp;</i></a></li>
+                  <li><a href="https://www.paypal.com/"><i class="fa fa-cc-paypal">&nbsp;</i></a></li>
+                                  <div class="clr"></div>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+      <div class="background_thingie"></div>
+     
+     
+      <div class="footer_partfour">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+              <p>Copyright &copy; 2020 <a href="https://sillicon14.com/">Silicon14 Inc. </a> All Rights Reserved.</p>
+            </div>
+
+            
+           
+          </div>
+        </div>
+      </div>
+   
+    </div>
+      </div>
+  </body>
+  );
+}
+
+export default App;
